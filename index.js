e399bd892f06a61ad7a29bd4baa218f1b2b7358d
Aissam Ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2023-11-17 00:40:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const URI = process.env.URI;
 console.log(`The value of MY_VARIABLE is: ${URI}`);
 
 let items = [];
+let itemsByVille = {};
+
 https.get(URI, resp => {
     var data = "";
 
@@ -16,16 +18,14 @@ https.get(URI, resp => {
     resp.on("end", () => {
       try {
         var items = JSON.parse(data);
+
+          items.forEach((item) => {
+            const ville = item.ville.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+            console.log(ville);
+          }
       } catch (error) {
         console.error("Error parsing JSON:", error);
       }   
-        let itemsByVille = {};
-        
-        // Group items by "ville"
-        data.forEach((item) => {
-        
-          const ville = item.ville.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-            console.log(ville);
     });
   })
   .on("error", err => {
