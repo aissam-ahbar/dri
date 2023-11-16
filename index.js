bde2cab1b036b9cc343c83d933f761cd01a7939e
Aissam Ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2023-11-17 00:21:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-// index.js
-
-const myVariable = process.env.URI;
+const https = require("https");
+const URI = process.env.URI;
 
 console.log(`The value of MY_VARIABLE is: ${myVariable}`);
+
+https.get(URI, resp => {
+    let data = "";
+
+    // A chunk of data has been recieved.
+    resp.on("data", chunk => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on("end", () => {
+      let data = JSON.parse(data);
+      console.log(JSON.stringify(data));      
+    });
+  })
+  .on("error", err => {
+    console.log("Error: " + err.message);
+  });
