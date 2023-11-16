d7e1740187c33865a362c5b0e89e710cd9689b9d
Aissam Ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2023-11-16 23:33:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-console.log("start program...", new Date());
+name: Greeting on variable day
 
-console.log(${{ vars.URI) }};
+on:
+  workflow_dispatch
+
+env:
+  DAY_OF_WEEK: $URI
+
+jobs:
+  greeting_job:
+    runs-on: ubuntu-latest
+    env:
+      Greeting: Hello
+    steps:
+      - name: "Say Hello Mona it's Monday"
+        run: echo "Today is $DAY_OF_WEEK"
+        env:
+          First_Name: Mona
